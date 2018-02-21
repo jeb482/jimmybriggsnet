3f689f91dd0a65b686a83e771dad15eec2e4f5b1
Jimmy B <jeb482@cornell.edu>
2018-02-21 18:40:43 -0500

    Updated Roma with youtube embed.

--- publications/roma.php
+++ publications/roma.php
@@ -23,39 +23,37 @@
     
     <div class="container">
        <div class="row jumbotron">
-        <div class="col-md-4"><img src="../images/roma_teaser.jpg"   class="img-fluid"></center></div>
-        <div class="col-md-8 col-md-offset-4 align-self-center" align="center">
-
-        <h1>RoMA: Interactive Fabrication with a Robotic Arm 3D Printer </h1>
+        <div class="align-self-center" align="center">
+        <h1>RoMA: Interactive Fabrication with Augmented Reality and a Robotic 3D Printer</h1>
         <p>Huaishu Peng, <strong>Jimmy Briggs</strong>, Cheng-Yao Wang, Kevin Guo, Joseph Kider, Stefanie Mueller, Patrick Baudisch, François Guimbretière. 
         <span class="text-muted"><a href="https://chi2018.acm.org/" target="_blank">CHI 2018</a> to appear.</span></p>
-        <p>
         <span class="badge badge-pill badge-c-sharp">C#</span>
         <span class="badge badge-pill badge-cpp">C++</span>
         <span class="badge badge-pill badge-opengl">OpenGL</span>
         <span class="badge badge-pill badge-openvr">OpenVR</span>
-        </p>
         </center>
         </div>
         </div>
+      <center> 
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/K_wWuYD1Fkg?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      </center>
       <h2>Abstract</h2>  
       <p>
-      We present the Robotic Modeling Assistant (RoMA), a system providing a fast, precise, 
-      hands-on modeling experience. As a designer creates a new design using RoMA AR CAD editor,
-      features are constructed concurrently by a 3D printing robotic arm sharing the same design
-      volume. The partially printed physical model then serves as a tangible reference for the
-      designer as she adds new elements to her design. RoMA’s proxemics-inspired handshake mechanism
-      between the designer and the 3D printing robotic arm allows the designer to quickly interrupt
-      printing to access a printed area or to indicate that the robot can take full control of the
-      model to finish printing. RoMA lets users integrate real-world constraints into a design rapidly,
-      allowing them to create well-proportioned cooking implements or to extend existing objects.
-      We conclude by presenting the strengths and limitations of our current design.
+      We present the Robotic Modeling Assistant (RoMA), aninteractive fabrication system providing
+      a fast, precise, hands-on and in-situ modeling experience. As a designer creates a new model
+      using RoMA AR CAD editor, features are constructed concurrentlyby a 3D printing robotic arm 
+      sharing the same design volume. The partially printed physical model then serves as a tangible
+      reference for the designer as she adds new elements to her design. RoMA’s proxemics-inspired
+      handshake mechanism between the designer and the 3D printing robotic arm allows the designer 
+      to quickly interrupt printing to access a printed area or to indicate that the robot can take
+      full control of the model to finish printing. RoMA lets users integrate real-world constraints
+      into a design rapidly,allowing them to create well-proportioned tangible artifactsor to extend
+      existing objects. We conclude by presenting the strengths and limitations of our current
+      design.
       </p>
       
       
       <center>
-      
-      <p><i>Video and more details coming soon.</i></p>
       </center>
 
     </div><!-- /.container -->
